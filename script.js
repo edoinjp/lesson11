b4a883e2966a4aed53c2487487ba810eaabb089f
edoinjp <edoinjp@gmail.com>
2021-10-06 05:20:32 -0800

    up

--- script.js
+++ script.js
@@ -7,11 +7,19 @@ function getCars() {
 getCars();
 
 // 2
-let names = ['ana','giorgi','nino','teona','irakli','eduard'];
+let names = ["ana", "giorgi", "nino", "teona", "irakli", "eduard"];
+
+// for (let i of names) {
+//   if (i > 3) {
+//     console.log(i);
+//   }
+// }
 
 for(let i=0 ;i<names.length;i++){
 	if(names[i].length>3) console.log(names[i]);
-		
-	
+
 }
-// 
+
+
+
+//

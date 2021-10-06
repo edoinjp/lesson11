bc506fd4535b989ccccc5d3b13e2b35826b4c018
edoinjp <edoinjp@gmail.com>
2021-10-06 03:33:12 -0800

    up

---
+++ script.js
@@ -0,0 +1,8 @@
+let cars = ["audi", "bmw", "lexus", "volkswagen", "porsche", "toyota"];
+function getCars() {
+  for (let i = 1; i < cars.length; i++) {
+    console.log(cars[i]);
+  }
+}
+getCars();
+console.log();
